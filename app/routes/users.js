cd5df3fb13f9891a46c8421212f53b64008335eb
Dax <limeside@gmail.com>
2016-08-12 11:57:46 -0500

    Add value toUser

--- app/routes/users.js
+++ app/routes/users.js
@@ -7,7 +7,6 @@ export default Ember.Route.extend({
       url:'https://q3api.herokuapp.com/api/v1/users',
       method:'GET'
     }).then( info => {
-      console.log(info);
       return {
         users:info,
         showContact:false
