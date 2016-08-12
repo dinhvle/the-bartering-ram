eb3a856617f916f28b6be18617a86c232f1eb746
Dax <limeside@gmail.com>
2016-08-11 22:07:44 -0500

    Add new post component. :rabbit:

--- app/components/new-listing.js
+++ app/components/new-listing.js
@@ -1,4 +1,30 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  session: Ember.inject.service(),
+  model(){
+    return{
+      user_id:null, //this.get('session').user_id
+      item_name:null,
+      item_desc:null,
+      img_url:null
+    }
+  },
+  actions:{
+    newPost(user,title,body,price,image){
+      Ember.$.ajax({
+        url:'http://127.0.0.1:3000/api/v1/listings',
+        method:'POST',
+        data:{
+          user_id:user,
+          item_name:title,
+          item_desc:body,
+          price:price,
+          img_url:image
+        }
+      }).then( () => {
+        console.log('New listing added.');
+      })
+    }
+  }
 });
